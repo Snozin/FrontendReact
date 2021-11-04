37c7ef4877f8b72b96115ad77abfcd4bdceb5ac3
Misael Duarte <msl.snzn@gmail.com>
2021-11-04 12:34:05 +0000

    Uso simple de classnames para renderizado condicional de clases

--- src/components/adverts/AdvertsPage.js
+++ src/components/adverts/AdvertsPage.js
@@ -1,32 +1,20 @@
-// const adverts = [
-//   {
-//     content:
-//       "Nos hace mucha ilusión anunciar la fecha del ESTRENO de 'Eso que tu me das', documental con la última entrevista a Pau Donés. 30 DE SEPTIEMBRE, en cines de toda España. @WarnerBrosSpain Y este es el cartel definitivo, con algunas frases de críticas que ya se han publicado.",
-//     userId: 1,
-//     updatedAt: "2021-03-15T18:23:57.579Z",
-//     id: 1,
-//   },
-//   {
-//     content:
-//       "'Soy muy fan tuya, pero ahora no me acuerdo cómo te llamas' (Una desconocida, en la calle).",
-//     userId: 1,
-//     updatedAt: "2021-03-15T18:24:56.773Z",
-//     id: 2,
-//   },
-// ]
 import { getAdverts } from "../services/dataService"
 import { useEffect, useState } from "react"
+import classNames from "classnames"
+
+import "./AdvertsPage.css"
 
 const AdvertsPage = () => {
   const [adverts, setAdverts] = useState([])
   useEffect(() => {
-    // Forma equivalente a la de abajo. 
+    // Forma equivalente a la de abajo.
     // then resuelve un único valor que le vamos a pasar a la función
     // getAdverts().then( data => setAdverts(data))
     getAdverts().then(setAdverts)
   }, [])
   return (
-    <div className="advertsPage">
+    // usando classNames para definir clases condicionalmente
+    <div className={classNames("advertsPage", { secondClass: adverts.length })}>
       <ul>
         {adverts.map((advert) => (
           <li key={advert.id}>{advert.content}</li>
