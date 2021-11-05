ef414f8ecc3dec1ddfb60d7291e712f2dd1da0bc
Misael Duarte <msl.snzn@gmail.com>
2021-11-05 07:22:52 +0000

    Agregar el token de autenticación a todas las peticiones por defecto con axios

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,12 @@
-import axios from 'axios'
+import axios from "axios"
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
-client.interceptors.response.use(response => response.data)
+client.interceptors.response.use((response) => response.data)
+
+export const setAuthorizationHeader = (token) => {
+  client.defaults.headers.common["Authorization"] = `Bearer ${token}`
+}
 export default client
--- src/components/auth/authDataService.js
+++ src/components/auth/authDataService.js
@@ -1,5 +1,12 @@
-import client from '../../api/client'
+import client, { setAuthorizationHeader } from "../../api/client"
 
-export const login = credentials => {
-  return client.post('/auth/login', credentials)
+export const login = (credentials) => {
+  /**
+   * La llamada al cliente axios devuelve una promesa. Al resolver esa promesa.
+   * Al resolver la promesa correctamente obtenemos el token y se lo pasamos
+   * a axios para que lo incluya en todas las peticiones siguientes
+   */
+  return client.post("/auth/login", credentials).then(({ accessToken }) => {
+    setAuthorizationHeader(accessToken)
+  })
 }
