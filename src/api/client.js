30d138ffa18528f7f2fe1010a1ac826186c81d64
Misael Duarte <msl.snzn@gmail.com>
2021-11-05 07:39:02 +0000

    Getión de errores con Axios

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,19 @@ const client = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
-client.interceptors.response.use((response) => response.data)
+client.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (!error.response) {
+      return Promise.reject({ message: error.message })
+    }
+    return Promise.reject({
+      message: error.response.statusText,
+      ...error.response,
+      ...error.response.data,
+    })
+  }
+)
 
 export const setAuthorizationHeader = (token) => {
   client.defaults.headers.common["Authorization"] = `Bearer ${token}`
