a99415f5df31813f929718a5449d84ec8a18a242
Misael Duarte <msl.snzn@gmail.com>
2021-11-05 05:16:12 +0000

    Refactor: usar un único useState para manejar los campos del formulario

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -3,15 +3,42 @@ import Button from "../../commons/Button"
 import "./LoginPage.css"
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [formValues, setFormValues] = useState({ username: "", password: "" })
+  // const [username, setUsername] = useState("")
+  // const [password, setPassword] = useState("")
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value)
-  }
+  /**
+   * Refactor para utilizar un único useState con un objeto que gestiona los
+   * distintos campos.
+   *
+   * Al setear el nuevo estado, como depende del estado anterior se usará la
+   * forma de función de setState(prevState => {newState})
+   */
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
+  // const handleUsernameChange = (event) => {
+  // setUsername(event.target.value)
+  // Usamos el spread operator para modificar únicamente la propiedad
+  // que nos interesa y preservar el resto tal cual estaba
+  //   setFormValues((prevState) => ({
+  //     ...prevState,
+  //     username: event.target.value,
+  //   }))
+  // }
+
+  // const handlePasswordChange = (event) => {
+  // setPassword(event.target.value)
+  //   setFormValues((prevState) => ({
+  //     ...prevState,
+  //     password: event.target.value,
+  //   }))
+  // }
+
+  const handleFormState = (event) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      // Lo que pongamos entre los corchetes se evalúa como clave del objeto
+      [event.target.name]: event.target.value,
+    }))
   }
 
   return (
@@ -21,19 +48,19 @@ const LoginPage = () => {
         <input
           type="text"
           name="username"
-          value={username}
-          onChange={handleUsernameChange}
+          value={formValues.username}
+          onChange={handleFormState}
         />
         <input
           type="password"
           name="password"
-          value={password}
-          onChange={handlePasswordChange}
+          value={formValues.password}
+          onChange={handleFormState}
         />
         <Button
           type="submit"
           variant="primary"
-          disabled={!username || !password}
+          disabled={!formValues.username || !formValues.password}
         >
           Log in
         </Button>
